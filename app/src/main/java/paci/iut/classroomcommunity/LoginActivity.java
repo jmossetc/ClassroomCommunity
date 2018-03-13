a14bd9c5b82b3a58376a71b382f1d34c9f21c8e7
jmossetc <jeremie.mossetchafik@gmail.com>
2018-03-13 15:43:59 +0100

    initial commit

---
+++ app/src/main/java/paci/iut/classroomcommunity/LoginActivity.java
@@ -0,0 +1,13 @@
+package paci.iut.classroomcommunity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
